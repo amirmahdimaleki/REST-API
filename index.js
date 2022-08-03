ca7e743e1c43c86644a35d6911abe0ddca7b081f
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-08-03 20:47:37 +0430

    db connection

--- index.js
+++ index.js
@@ -2,28 +2,34 @@
 
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
 const session = require('express-session')
 const connectDB = require("./db/connect");
 
 const app = express()
-const PORT = process.env.PORT || 5000
+
 
 
 app.get("/", (req, res)=>{
     res.send("REST API!")
 })
 
+
+
+
+// * app.listen (server start)
+
+const PORT = process.env.PORT || 5000;
+
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI);
-  
-      app.listen(PORT, () => {
-        console.log(`server is running on port ${PORT}`);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  start();
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
