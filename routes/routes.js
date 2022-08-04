1153069e65db99100e954cfc96db3be08b2c68b7
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-08-04 20:32:35 +0430

    it has some errors with ejs file

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    res.send('Heme pg')
+    res.render("index", {title: " home page"})
 })
 
 module.exports = router
